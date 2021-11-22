b6d0c7c43d1cfd192105850bc4b935cb0cbaafa0
megrogan <megrogan@gmail.com>
2021-11-22 11:19:46 +0000

    Initial design notes (#1)

--- backend/canisters/bucket/api/src/main.rs
+++ backend/canisters/bucket/api/src/main.rs
@@ -1,7 +1,7 @@
 use candid_gen::generate_candid_method;
 
 fn main() {
-    generate_candid_method!(data_bucket, put_chunk, update);
+    generate_candid_method!(bucket, upload_chunk, update);
 
     candid::export_service!();
     std::print!("{}", __export_service());
