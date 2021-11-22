13ca0fc446960ca9f487d1a3d07de6363968ba44
megrogan <megrogan@gmail.com>
2021-11-22 14:40:30 +0000

    Index API

---
+++ backend/canisters/index/api/src/updates/c2c_add_blob_reference.rs
@@ -0,0 +1,15 @@
+use candid::{CandidType, Principal};
+use serde::Deserialize;
+
+#[derive(CandidType, Deserialize, Debug)]
+pub struct Args {
+    pub principal: Principal,
+    pub blob_size: u64,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub enum Response {
+    Success,
+    AllowanceReached,
+    UserNotFound,
+}
---
+++ backend/canisters/index/api/src/updates/mod.rs
@@ -0,0 +1,6 @@
+pub mod add_user;
+pub mod c2c_add_blob_reference;
+pub mod c2c_remove_blob_reference;
+pub mod delete_accessor;
+pub mod delete_user;
+pub mod update_user;
