adaf073e3dddabaec92c97115b3545fac6a3e020
megrogan <megrogan@gmail.com>
2021-11-23 10:18:13 +0000

    user:index implement add_user, update_user, user (#6)

---
+++ backend/canisters/index/impl/src/queries/user.rs
@@ -0,0 +1,22 @@
+use crate::{RuntimeState, RUNTIME_STATE};
+use canister_api_macros::trace;
+use ic_cdk_macros::query;
+use index_canister::user::*;
+
+#[query]
+#[trace]
+fn user(_args: Args) -> Response {
+    RUNTIME_STATE.with(|state| user_impl(state.borrow_mut().as_mut().unwrap()))
+}
+
+fn user_impl(runtime_state: &mut RuntimeState) -> Response {
+    let user_id = runtime_state.env.caller();
+    if let Some(user) = runtime_state.data.users.get(&user_id) {
+        Response::Success(UserRecord {
+            bytes_used: user.bytes_used,
+            byte_limit: user.byte_limit,
+        })
+    } else {
+        Response::UserNotFound
+    }
+}
