4c1be998d49e4af3306b9615b0b1ff660cf1c9a2
megrogan <megrogan@gmail.com>
2021-11-26 11:29:03 +0000

    Add a backend build script

---
+++ build_backend.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "1. cargo build"
+cargo build
+
+echo "2. cargo clippy"
+cargo clippy
+
+echo "3. cargo fmt"
+cargo fmt
+
+echo "4. cargo test"
+cargo test --lib
+
+echo "5. validate candid syntax"
+source validate-candid-syntax.sh
+
+echo "6. validate candid matches rust"
+source validate-candid-matches-rust.sh
