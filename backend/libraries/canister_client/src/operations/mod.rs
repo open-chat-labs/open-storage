249c5c0985fa7c0e25b8eb354c3b357770af7bbf
Hamish Peebles <hamishpeebles@gmail.com>
2021-11-27 15:39:28 +0000

    Add scripts to upgrade canisters on local or prod

--- backend/libraries/canister_client/src/operations/mod.rs
+++ backend/libraries/canister_client/src/operations/mod.rs
@@ -1,3 +1,5 @@
 mod install_service_canisters;
+mod upgrade_service_canisters;
 
 pub use install_service_canisters::*;
+pub use upgrade_service_canisters::*;
---
+++ upgrade-canister-local.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+IDENTITY=$1
+CANISTER_TO_UPGRADE=$2
+VERSION=$3
+
+# Pass in the dfx identity name
+# eg './upgrade-canister-local openstorage index 1.0.0'
+./generate-wasm.sh index_canister_impl
+./generate-wasm.sh bucket_canister_impl
+
+./compress-wasm.sh bucket_canister_impl
+
+INDEX_CANISTER_ID=$(dfx canister --no-wallet id user_index)
+
+cargo run \
+  --manifest-path backend/canister_upgrader/Cargo.toml \
+  'http://127.0.0.1:8000/' \
+  $IDENTITY \
+  $INDEX_CANISTER_ID \
+  $CANISTER_TO_UPGRADE \
+  $VERSION \
