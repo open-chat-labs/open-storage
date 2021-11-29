b36b0159de4d0cc6463c9f689226d2e748dbfe54
Hamish Peebles <hamishpeebles@gmail.com>
2021-11-29 14:43:39 +0000

    Merge add_user and update_user into add_or_update_users (#37)

--- backend/canisters/index/api/src/main.rs
+++ backend/canisters/index/api/src/main.rs
@@ -4,10 +4,9 @@ fn main() {
     generate_candid_method!(index, allocated_bucket, query);
     generate_candid_method!(index, user, query);
 
-    generate_candid_method!(index, add_user, update);
+    generate_candid_method!(index, add_or_update_users, update);
     generate_candid_method!(index, remove_accessor, update);
     generate_candid_method!(index, remove_user, update);
-    generate_candid_method!(index, update_user, update);
 
     candid::export_service!();
     std::print!("{}", __export_service());
--- backend/canisters/index/api/src/updates/add_or_update_users.rs
+++ backend/canisters/index/api/src/updates/add_or_update_users.rs
@@ -4,12 +4,16 @@ use types::UserId;
 
 #[derive(CandidType, Deserialize, Debug)]
 pub struct Args {
+    pub users: Vec<UserConfig>,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub struct UserConfig {
     pub user_id: UserId,
-    pub byte_limit: Option<u64>,
+    pub byte_limit: u64,
 }
 
 #[derive(CandidType, Deserialize, Debug)]
 pub enum Response {
     Success,
-    UserNotFound,
 }
