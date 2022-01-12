687261ee97f80b1fa0a4f1162f32814d59c582fd
Hamish Peebles <hamishpeebles@gmail.com>
2022-01-12 10:18:39 +0000

    Make cycles check only happen once every 10 minutes (#53)

--- backend/libraries/cycles_utils/src/check_cycles_balance.rs
+++ backend/libraries/cycles_utils/src/check_cycles_balance.rs
@@ -1,9 +1,17 @@
 use canister_client_macros::generate_c2c_call;
+use std::cell::Cell;
 use tracing::error;
-use types::{CanisterId, Cycles};
+use types::{CanisterId, Cycles, Milliseconds, TimestampMillis};
+use utils::time::MINUTE_IN_MS;
 
-pub fn check_cycles_balance(min_cycles_balance: Cycles, top_up_canister_id: CanisterId) {
-    if should_notify(min_cycles_balance) {
+const CYCLES_CHECK_INTERVAL: Milliseconds = 10 * MINUTE_IN_MS; // 10 minutes
+
+thread_local! {
+    static LAST_CHECKED: Cell<TimestampMillis> = Cell::default();
+}
+
+pub fn check_cycles_balance(min_cycles_balance: Cycles, top_up_canister_id: CanisterId, now: TimestampMillis) {
+    if is_cycles_check_due(now) && should_notify(min_cycles_balance) {
         ic_cdk::block_on(send_notification(top_up_canister_id));
     }
 }
@@ -12,6 +20,17 @@ fn should_notify(min_cycles_balance: Cycles) -> bool {
     let cycles_balance: Cycles = ic_cdk::api::canister_balance().into();
 
     cycles_balance < min_cycles_balance
+}
+
+fn is_cycles_check_due(now: TimestampMillis) -> bool {
+    LAST_CHECKED.with(|t| {
+        if now > t.get() + CYCLES_CHECK_INTERVAL {
+            t.set(now);
+            true
+        } else {
+            false
+        }
+    })
 }
 
 async fn send_notification(canister_id: CanisterId) {
