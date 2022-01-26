5c57dcc258b85cd201ae4e12990f11dc08d3f19c
Hamish Peebles <hamishpeebles@gmail.com>
2022-01-26 16:02:07 +0000

    Remove deserialization attributes from shared types (#61)

--- backend/libraries/types/src/file.rs
+++ backend/libraries/types/src/file.rs
@@ -4,28 +4,22 @@ use serde::{Deserialize, Serialize};
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct FileAdded {
-    #[serde(rename(deserialize = "blob_id"))]
     pub file_id: FileId,
     pub uploaded_by: UserId,
-    #[serde(rename(deserialize = "blob_hash"))]
     pub hash: Hash,
-    #[serde(rename(deserialize = "blob_size"))]
     pub size: u64,
 }
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct FileRemoved {
-    #[serde(default)]
     pub file_id: FileId,
     pub uploaded_by: UserId,
-    #[serde(rename(deserialize = "blob_hash"))]
     pub hash: Hash,
     pub blob_deleted: bool,
 }
 
 #[derive(CandidType, Deserialize, Debug)]
 pub struct FileRejected {
-    #[serde(rename(deserialize = "blob_id"))]
     pub file_id: FileId,
     pub reason: FileRejectedReason,
 }
