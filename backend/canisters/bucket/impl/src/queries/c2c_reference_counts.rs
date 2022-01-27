d7cc2f62f6bb751938a61fd979627bbfb85d877a
Hamish Peebles <hamishpeebles@gmail.com>
2022-01-27 11:14:52 +0000

    Remove guard so that we can test the live buckets before running the index refresh

--- backend/canisters/bucket/impl/src/queries/c2c_reference_counts.rs
+++ backend/canisters/bucket/impl/src/queries/c2c_reference_counts.rs
@@ -1,10 +1,9 @@
-use crate::guards::caller_is_index_canister;
 use crate::{read_state, RuntimeState};
 use bucket_canister::c2c_reference_counts::{Response::*, *};
 use canister_api_macros::trace;
 use ic_cdk_macros::update;
 
-#[update(guard = "caller_is_index_canister")]
+#[update]
 #[trace]
 fn c2c_reference_counts(_args: Args) -> Response {
     read_state(c2c_reference_counts_impl)
