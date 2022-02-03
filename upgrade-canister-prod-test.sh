e2a05f1973929b09aa7e030347a88c3623ca184c
Hamish Peebles <hamishpeebles@gmail.com>
2022-02-03 18:35:57 +0000

    Add scripts and config for new test env on mainnet

--- upgrade-canister-prod-test.sh
+++ upgrade-canister-prod-test.sh
@@ -5,17 +5,17 @@ CANISTER_TO_UPGRADE=$2
 VERSION=$3
 
 # Pass in the dfx identity name
-# eg './upgrade-canister-local.sh openstorage index 1.0.0'
+# eg './upgrade-canister-prod-test.sh openstorage index 1.0.0'
 ./generate-wasm.sh index_canister_impl
 ./generate-wasm.sh bucket_canister_impl
 
 ./compress-wasm.sh bucket_canister_impl
 
-INDEX_CANISTER_ID=$(dfx canister --no-wallet id index)
+INDEX_CANISTER_ID=$(dfx canister --network ic_test --no-wallet id index)
 
 cargo run \
   --manifest-path backend/canister_upgrader/Cargo.toml \
-  'http://127.0.0.1:8000/' \
+  'https://ic0.app/' \
   $IDENTITY \
   $INDEX_CANISTER_ID \
   $CANISTER_TO_UPGRADE \
