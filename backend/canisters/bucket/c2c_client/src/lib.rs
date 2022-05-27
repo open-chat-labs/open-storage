320b1c64c0dff5cefdfdfa0e15352f4add18a9e2
Hamish Peebles <hamishpeebles@gmail.com>
2022-05-27 18:24:47 +0100

    Expose all methods via `c2c_client`

--- backend/canisters/bucket/c2c_client/src/lib.rs
+++ backend/canisters/bucket/c2c_client/src/lib.rs
@@ -2,6 +2,10 @@ use bucket_canister::*;
 use canister_client_macros::*;
 
 // Queries
+generate_c2c_call!(file_status);
 
 // Updates
 generate_c2c_call!(c2c_sync_index);
+generate_c2c_call!(delete_file);
+generate_c2c_call!(delete_files);
+generate_c2c_call!(upload_chunk_v2);
