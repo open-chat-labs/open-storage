57a10527469678101eeda2b296d62fc7219b2f7f
Hamish Peebles <hamishpeebles@gmail.com>
2022-05-31 10:14:53 +0100

    Expose all methods via `c2c_client` (#105)

--- backend/canisters/index/c2c_client/src/lib.rs
+++ backend/canisters/index/c2c_client/src/lib.rs
@@ -2,6 +2,14 @@ use canister_client_macros::*;
 use index_canister::*;
 
 // Queries
+generate_c2c_call!(allocated_bucket_v2);
+generate_c2c_call!(user);
 
 // Updates
+generate_c2c_call!(add_or_update_users);
+generate_c2c_call!(add_service_principals);
+generate_c2c_call!(c2c_notify_low_balance);
 generate_c2c_call!(c2c_sync_bucket);
+generate_c2c_call!(remove_accessor);
+generate_c2c_call!(remove_user);
+generate_c2c_call!(update_bucket_canister_wasm);
