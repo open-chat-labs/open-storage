23b5cbbc3fa9dd05804010d1a13596e4fa8263f2
Hamish Peebles <hamishpeebles@gmail.com>
2022-06-21 11:42:29 +0100

    Remove the deprecated `allocated_bucket`

--- backend/canisters/index/api/src/queries/allocated_bucket_v2.rs
+++ backend/canisters/index/api/src/queries/allocated_bucket_v2.rs
@@ -1,4 +1,3 @@
-use crate::allocated_bucket;
 use crate::ProjectedAllowance;
 use candid::CandidType;
 use serde::{Deserialize, Serialize};
@@ -26,22 +25,4 @@ pub struct SuccessResult {
     pub bytes_used: u64,
     pub bytes_used_after_upload: u64,
     pub projected_allowance: ProjectedAllowance,
-}
-
-impl From<allocated_bucket::Response> for Response {
-    fn from(response: allocated_bucket::Response) -> Self {
-        match response {
-            allocated_bucket::Response::AllowanceExceeded(pa) => Response::AllowanceExceeded(pa),
-            allocated_bucket::Response::BucketUnavailable => Response::BucketUnavailable,
-            allocated_bucket::Response::UserNotFound => Response::UserNotFound,
-            allocated_bucket::Response::Success(sr) => Response::Success(SuccessResult {
-                canister_id: sr.canister_id,
-                chunk_size: sr.chunk_size,
-                byte_limit: sr.projected_allowance.byte_limit,
-                bytes_used: sr.projected_allowance.bytes_used,
-                bytes_used_after_upload: sr.projected_allowance.bytes_used_after_operation,
-                projected_allowance: sr.projected_allowance,
-            }),
-        }
-    }
 }
