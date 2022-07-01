083e654ba78fc65c890ea46b7797a435504f1d31
Hamish Peebles <hamishpeebles@gmail.com>
2022-07-01 10:09:06 +0100

    Add `update_user_id` to candid file

--- backend/canisters/index/api/src/main.rs
+++ backend/canisters/index/api/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     generate_candid_method!(index, add_or_update_users, update);
     generate_candid_method!(index, remove_accessor, update);
     generate_candid_method!(index, remove_user, update);
+    generate_candid_method!(index, update_user_id, update);
 
     candid::export_service!();
     std::print!("{}", __export_service());
