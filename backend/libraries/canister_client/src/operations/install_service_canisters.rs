abc40df4318b3173b541361b0a90050a416421da
Hamish Peebles <hamishpeebles@gmail.com>
2023-01-17 12:11:18 +0000

    Integrate with `cycles_dispenser`

--- backend/libraries/canister_client/src/operations/install_service_canisters.rs
+++ backend/libraries/canister_client/src/operations/install_service_canisters.rs
@@ -4,6 +4,7 @@ use candid::Principal;
 use ic_agent::identity::BasicIdentity;
 use ic_agent::Identity;
 use ic_utils::interfaces::ManagementCanister;
+use index_canister::init::CyclesDispenserConfig;
 use types::{CanisterId, Version};
 
 pub async fn create_and_install_service_canisters(identity: BasicIdentity, url: String, test_mode: bool) -> CanisterId {
@@ -15,23 +16,37 @@ pub async fn create_and_install_service_canisters(identity: BasicIdentity, url: 
 
     println!("index canister id: {index_canister_id}");
 
-    install_service_canisters_impl(principal, &index_canister_id, &management_canister, test_mode).await;
+    install_service_canisters_impl(principal, &index_canister_id, &management_canister, None, test_mode).await;
 
     index_canister_id
 }
 
-pub async fn install_service_canisters(identity: BasicIdentity, url: String, index_canister_id: CanisterId, test_mode: bool) {
+pub async fn install_service_canisters(
+    identity: BasicIdentity,
+    url: String,
+    index_canister_id: CanisterId,
+    cycles_dispenser_config: Option<CyclesDispenserConfig>,
+    test_mode: bool,
+) {
     let principal = identity.sender().unwrap();
     let agent = build_ic_agent(url, identity).await;
     let management_canister = build_management_canister(&agent);
 
-    install_service_canisters_impl(principal, &index_canister_id, &management_canister, test_mode).await;
+    install_service_canisters_impl(
+        principal,
+        &index_canister_id,
+        &management_canister,
+        cycles_dispenser_config,
+        test_mode,
+    )
+    .await;
 }
 
 async fn install_service_canisters_impl(
     principal: Principal,
     index_canister_id: &CanisterId,
     management_canister: &ManagementCanister<'_>,
+    cycles_dispenser_config: Option<CyclesDispenserConfig>,
     test_mode: bool,
 ) {
     let version = Version::min();
@@ -41,6 +56,7 @@ async fn install_service_canisters_impl(
     let index_init_args = index_canister::init::Args {
         service_principals: vec![principal],
         bucket_canister_wasm,
+        cycles_dispenser_config,
         wasm_version: Version::min(),
         test_mode,
     };
