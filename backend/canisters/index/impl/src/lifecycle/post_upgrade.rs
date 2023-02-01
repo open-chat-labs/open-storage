df3722bf7d9049ba259f9ce12c83637c231b086c
Hamish Peebles <hamishpeebles@gmail.com>
2023-02-01 11:26:44 +0000

    Remove code needed for previous upgrade

--- backend/canisters/index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/index/impl/src/lifecycle/post_upgrade.rs
@@ -1,26 +1,23 @@
 use crate::lifecycle::{init_cycles_dispenser_client, init_state, BUFFER_SIZE};
+use crate::memory::get_upgrades_memory;
 use crate::Data;
 use canister_api_macros::trace;
 use canister_logger::LogEntry;
-use ic_cdk::api::stable::BufferedStableReader;
 use ic_cdk_macros::post_upgrade;
+use ic_stable_structures::reader::{BufferedReader, Reader};
 use index_canister::post_upgrade::Args;
-use serde::Deserialize;
 use tracing::info;
-use types::TimestampMillis;
 use utils::env::canister::CanisterEnv;
 
 #[post_upgrade]
 #[trace]
 fn post_upgrade(args: Args) {
     let env = Box::new(CanisterEnv::new());
-    let reader = BufferedStableReader::new(BUFFER_SIZE);
 
-    let (data, log_messages, trace_messages): (Data, Vec<LogMessage>, Vec<LogMessage>) =
-        serializer::deserialize(reader).unwrap();
+    let memory = get_upgrades_memory();
+    let reader = BufferedReader::new(BUFFER_SIZE, Reader::new(&memory, 0));
 
-    let logs = log_messages.into_iter().map(|m| m.into()).collect();
-    let traces = trace_messages.into_iter().map(|m| m.into()).collect();
+    let (data, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>) = serializer::deserialize(reader).unwrap();
 
     canister_logger::init_with_logs(data.test_mode, logs, traces);
 
@@ -31,20 +28,4 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
-}
-
-#[derive(Deserialize)]
-pub struct LogMessage {
-    pub timestamp: TimestampMillis,
-    #[serde(alias = "json")]
-    pub message: String,
-}
-
-impl From<LogMessage> for LogEntry {
-    fn from(value: LogMessage) -> Self {
-        LogEntry {
-            timestamp: value.timestamp,
-            message: value.message,
-        }
-    }
 }
