f061eefe12d0cb40f0210727b699bde6f8e93f5d
Hamish Peebles <hamishpeebles@gmail.com>
2023-02-07 18:03:23 +0000

    Use `raw_rand` to seed rng

--- backend/canisters/bucket/impl/src/lifecycle/init.rs
+++ backend/canisters/bucket/impl/src/lifecycle/init.rs
@@ -1,8 +1,9 @@
-use crate::lifecycle::init_state;
+use crate::lifecycle::{init_state, reseed_rng};
 use crate::Data;
 use bucket_canister::init::Args;
 use canister_api_macros::trace;
 use ic_cdk_macros::init;
+use std::time::Duration;
 use tracing::info;
 use utils::env::canister::CanisterEnv;
 use utils::env::Environment;
@@ -12,13 +13,15 @@ use utils::env::Environment;
 fn init(args: Args) {
     canister_logger::init(args.test_mode);
 
-    let env = Box::new(CanisterEnv::new());
+    let env = Box::new(CanisterEnv::new_insecure());
 
     let index_canister_id = env.caller();
 
     let data = Data::new(index_canister_id, env.now(), args.test_mode);
 
     init_state(env, data, args.wasm_version);
+
+    ic_cdk::timer::set_timer(Duration::default(), reseed_rng);
 
     info!(version = %args.wasm_version, "Initialization complete");
 }
